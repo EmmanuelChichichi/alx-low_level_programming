4d289e3cf00f6ffafbf379b50934ad5cef1ff4f4
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-16 04:55:24 -0700

    i kind of get it

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char strg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	write(2, strg, 59);
+	return (1);
+}
