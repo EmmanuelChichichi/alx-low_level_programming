e2086e82167654cb833f15196a877b08bfb14260
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-17 08:23:20 -0700

    threes

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print threes
+ *
+ * Return: ALways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
