ad53325627c43966d8629dd6b77c14da10b067ff
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-17 05:47:25 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print single digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
