2090d53430ede2cec6eaf398f2c33568173e1210
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-20 07:21:06 -0700

    alpha code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lower case
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
