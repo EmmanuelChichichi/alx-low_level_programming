31fcba7dc30ed545b2881081320f133913a349a4
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-21 04:41:00 -0700

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,21 +7,19 @@
 
 void jack_bauer(void)
 {
-	int i, a, b;
+	int a, b;
 
-	for (i = 0; i <= 1440; i++)
+	for (a = 0; a < 24; a++)
 	{
-		for (a = 0; a < 24; a++)
+		for (b = 0; b < 60; b++)
 		{
-			for (b = 0; b < 60; b++)
-			{
-				_putchar(a / 10 + '0');
-				_putchar(a % 10 + '0');
-				_putchar(':');
-				_putchar(b / 10 + '0');
-				_putchar(b % 10 + '0');
-				_putchar('\n');
-			}
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
 		}
 	}
+
 }
