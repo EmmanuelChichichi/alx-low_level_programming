9e9ab4373742740ed688db4c12124ba585445205
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-21 03:41:52 -0700

    absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
