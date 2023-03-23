b95d8eb9172bc69aacfb05b95072e3f232b16220
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-23 03:10:38 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies twi integers
+ * @a: variable to be multiplied
+ * @b: variable to be multiplied
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
