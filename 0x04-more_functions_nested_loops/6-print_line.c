797ebcb8ff909f6296e7692cd3af345f76400b66
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-23 03:21:25 -0700

    thanks gpt

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,14 @@ void print_line(int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (i = 0; i <= n; i++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
 		_putchar(95);
 	}
 	_putchar('\n');
