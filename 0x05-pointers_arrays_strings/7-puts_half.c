e84c9a445f63b5e3add797204766fba4a3200698
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-28 03:48:04 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: variable to be evaluated
+ */
+
+void puts_half(char *str)
+{
+	int c = 0, i;
+
+	while (c >= 0)
+	{
+		if (str[c] == '\0')
+			break;
+		c++;
+	}
+
+	if (c % 2 == 1)
+		i = c / 2;
+	else
+		i = (c - 1) / 2;
+
+	for (i++; i < c; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
