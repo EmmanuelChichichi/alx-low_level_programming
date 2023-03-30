8b9e2e4945480f5e8f91ab406e29a20ae10c2619
Emmanuel <emmanuelchichichi27@gmail.com>
2023-03-30 02:51:20 -0700

    null terminatorgit add 1-strncat.c!

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncat - links two strings
+ * @dest: string to be linked
+ * @src: string to be linked
+ * @n: integer representing elements
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *app = dest;
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		app++;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		app[j] = src[j];
+	}
+	app[j] = '\0';
+	return (dest);
+}
