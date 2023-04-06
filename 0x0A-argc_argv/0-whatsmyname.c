b393680010410e820d50be715f4bd2c823f31ec8
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-06 04:19:52 -0700

    my name is Emmanuel

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints name followed by a new line
+ * @argc: number of arguments
+ * @argv: points to strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
