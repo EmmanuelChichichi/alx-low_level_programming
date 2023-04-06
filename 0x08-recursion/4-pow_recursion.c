46d9548d132cb592313f1d6650ee2b3ef36d996e
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-06 01:41:50 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: base digit
  * @y: indice
- * return: value
+ * Return: x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
