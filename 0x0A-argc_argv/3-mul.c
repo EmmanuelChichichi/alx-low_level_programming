b2e538381a6b1e156bb8eee1932a30cd0d846457
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-07 02:28:57 -0700

    what is mul.c even

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc >= 2)
+	if (argc >= 3)
 	{
-		mul = atoi(argv[1]) * atoi( argv[2]);
+		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 		return (0);
 	}
