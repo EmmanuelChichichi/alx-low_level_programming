8c673be82fcfe6b32ba0211fa44841ae923bfe78
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-19 04:50:42 -0700

    hide and seek; integer edition

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: pointer to an array
+ * @size: number of elements in array
+ * @cmp: pointer to the function
+ * Return: integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
