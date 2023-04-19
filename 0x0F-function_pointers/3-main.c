46e05613b1461485014c5e3260c8cf2905521c9d
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-19 15:40:06 -0700

    im pissed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,16 +1,16 @@
-#include "3-calc.h"
+nclude "3-calc.h"
 
 /**
  * main - check the code for Holberton School students.
  * @argc: argument count.
  * @argv: argument vector.
+ *
  * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int a, b, ans;
-	int (*op)(int, int);
+	int a, b;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -18,24 +18,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if (b == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	op = get_op_func(argv[2]);
-
-	if (op == NULL)
-	{
-		 printf("Error\n");
-		 exit(99);
-	}
-
-	ans = op(a, b);
-
-	printf("%d\n", ans);
+	printf("%d\n", operation(a, b));
 	return (0);
 }
