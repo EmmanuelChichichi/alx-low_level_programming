c4283e89c06cd1d1996f1e09c5a5cffaa55393ee
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-19 04:04:45 -0700

    tell me what my name is OH Cgit add 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
