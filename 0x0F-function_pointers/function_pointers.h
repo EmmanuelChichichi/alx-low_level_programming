8e67b495d420aa0e884b4e724ddc8865b52ae534
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-20 02:35:25 -0700

    this better be it

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,10 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 #include <stdlib.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 void print_name(char *name, void (*f)(char *));
-
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
