e0e848d818bca377c9e289fe9e7321232e6622c3
Emmanuel <emmanuelchichichi27@gmail.com>
2023-04-28 16:13:19 -0700

    let my elements gogit add 4-free_list.c!

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the linked list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(curr->str);
+		free(curr);
+	}
+}
