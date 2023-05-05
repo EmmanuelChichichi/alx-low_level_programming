4497e3588988e81e3c2107b91b39a6434b58f39f
Emmanuel <emmanuelchichichi27@gmail.com>
2023-05-05 05:34:50 -0700

    if you're a developer, turn these binary numbers into unsigned integers

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-unsignied int binary_to_uint(const char *b);
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
 
 void print_binary(unsigned long int n);
 
