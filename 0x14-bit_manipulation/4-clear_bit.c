c2100ff8c4d28cdf9bdd1059637643ea6a175809
Emmanuel <emmanuelchichichi27@gmail.com>
2023-07-06 03:51:02 -0700

    nothing suspicious at all kimba

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit -  sets the value of a bit to 0 at a given index
+ * @n: again, another random integer
+ * @index: area where the bit is placed
+ * Return: 1 if it worked, or -1 if an error occurred
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n &= ~(1ul << index);
+	return (1);
+}
